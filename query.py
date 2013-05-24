a3b8ee52d5cc4606f97436a37b6dbb26e7be6d9d
Simon Howe <footless@gmail.com>
2013-05-24 16:30:09 +0200

    Skeleton for csv query

---
+++ query.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+def query(filename, column_index, value):
+    return []
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Query a CSV file')
+    parser.add_argument('filename', help='CSV file')
+    parser.add_argument('column_index', help='The column index to search')
+    parser.add_argument('value', help='The value to search for')
+    args = parser.parse_args()
+
+    rows = query(args.filename, args.column_index, args.value)
+
+    for r in rows:
+        print r
